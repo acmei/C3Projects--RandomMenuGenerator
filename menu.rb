2400ff0348a9382d58dab6b5b551039081edca8f
Anita <asovay@gmail.com>
2015-05-17 09:03:24 -0700

    Additional alignment adjustment

--- menu.rb
+++ menu.rb
@@ -5,13 +5,13 @@ menu_items = gets.chomp.to_i
 
 # Create 3 arrays, 1. food adjectives, 2. food preparation 3. foods (aka rare animals)
 adj 		= ["Melty", "Spicy", "Buttered", "Gourmet", "Fresh", "Hot", "Crunchy", "Sweet", "Savory", 
-			"Marinated", "Chocolatey", "Flaming", "Creamy", "Polka-dotted", "Chunky"]
+		"Marinated", "Chocolatey", "Flaming", "Creamy", "Polka-dotted", "Chunky"]
 prep 		= ["pan-fried", "sauteed", "steamed", "roasted", "baked", "boiled", "broiled", "fried", 
-			"mashed", "breaded", "scorched", "barbecue", "slow-roasted", "braised", "chopped", "raw", 
-			"sous vide", "poached"]
+		"mashed", "breaded", "scorched", "barbecue", "slow-roasted", "braised", "chopped", "raw", 
+		"sous vide", "poached"]
 rare_animals 	= ["panda", "unicorn", "manatee", "wooly mammoth", "bald eagle", "gorilla", "emperor penguin", 
-			"blue whale", "hammerhead shark", "toucan", "chimpanzee", "hummingbird", "sea turtle", "rhino", 
-			"monarch butterfly", "sloth"]
+		"blue whale", "hammerhead shark", "toucan", "chimpanzee", "hummingbird", "sea turtle", "rhino", 
+		"monarch butterfly", "sloth"]
 
 # Print menu items in an ordered list based on requested number of items by user
 count = 0
