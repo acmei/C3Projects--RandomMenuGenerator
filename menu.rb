6429368cf68bae5cab7e9474c13b054e0f71ae3a
Anita <asovay@gmail.com>
2015-06-11 11:31:32 -0700

    Incorporated comments

--- menu.rb
+++ menu.rb
@@ -1,17 +1,24 @@
 # How many menu items would the user like to see?
 puts "Welcome to the crazy random menu generator!"
-puts "How many menu items would you like to create?"
-menu_items = gets.chomp.to_i
+print "How many menu items would you like to create? "
+
+menu_items = gets.chomp
+
+# Invalid input handling - no input, floats, or letters
+while menu_items == "" || menu_items.to_i == 0
+  puts "Please type a valid number."
+  menu_items = gets.chomp.to_i
+end
 
 # Create 3 arrays, 1. food adjectives, 2. food preparation 3. foods (aka rare animals)
-adj 		= ["Melty", "Spicy", "Buttered", "Gourmet", "Fresh", "Hot", "Crunchy", "Sweet", "Savory", 
-			"Marinated", "Chocolatey", "Flaming", "Creamy", "Polka-dotted", "Chunky"]
-prep 		= ["pan-fried", "sauteed", "steamed", "roasted", "baked", "boiled", "broiled", "fried", 
-			"mashed", "breaded", "scorched", "barbecue", "slow-roasted", "braised", "chopped", "raw", 
-			"sous vide", "poached"]
-rare_animals 	= ["panda", "unicorn", "manatee", "wooly mammoth", "bald eagle", "gorilla", "emperor penguin", 
-			"blue whale", "hammerhead shark", "toucan", "chimpanzee", "hummingbird", "sea turtle", "rhino", 
-			"monarch butterfly", "sloth"]
+adj 	         = ["Melty", "Spicy", "Buttered", "Gourmet", "Fresh", "Hot", "Crunchy", "Sweet", "Savory", 
+			           "Marinated", "Chocolatey", "Flaming", "Creamy", "Polka-dotted", "Chunky"]
+prep 		       = ["pan-fried", "sauteed", "steamed", "roasted", "baked", "boiled", "broiled", "fried", 
+			           "mashed", "breaded", "scorched", "barbecue", "slow-roasted", "braised", "chopped", "raw", 
+			           "sous vide", "poached"]
+rare_animals 	 = ["panda", "unicorn", "manatee", "wooly mammoth", "bald eagle", "gorilla", "emperor penguin", 
+			           "blue whale", "hammerhead shark", "toucan", "chimpanzee", "hummingbird", "sea turtle", "rhino", 
+			           "monarch butterfly", "sloth"]
 
 # Print menu items in an ordered list based on requested number of items by user
 count = 0
