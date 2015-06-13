e9a830289bbd36aac50be4aabdd85dcc451d8f67
Anita <asovay@gmail.com>
2015-06-13 00:31:58 -0700

    beautified

--- menu.rb
+++ menu.rb
@@ -11,18 +11,18 @@ while menu_items == "" || menu_items.to_i == 0
 end
 
 # Create 3 arrays, 1. food adjectives, 2. food preparation 3. foods (aka rare animals)
-adj 	         = ["Melty", "Spicy", "Buttered", "Gourmet", "Fresh", "Hot", "Crunchy", "Sweet", "Savory", 
-			           "Marinated", "Chocolatey", "Flaming", "Creamy", "Polka-dotted", "Chunky"]
-prep           = ["pan-fried", "sauteed", "steamed", "roasted", "baked", "boiled", "broiled", "fried", 
-			           "mashed", "breaded", "scorched", "barbecue", "slow-roasted", "braised", "chopped", "raw", 
-			           "sous vide", "poached"]
-rare_animals 	 = ["panda", "unicorn", "manatee", "wooly mammoth", "bald eagle", "gorilla", "emperor penguin", 
-			           "blue whale", "hammerhead shark", "toucan", "chimpanzee", "hummingbird", "sea turtle", "rhino", 
-			           "monarch butterfly", "sloth"]
+adj           = ["Melty", "Spicy", "Buttered", "Gourmet", "Fresh", "Hot", "Crunchy", "Sweet", "Savory",
+                "Marinated", "Chocolatey", "Flaming", "Creamy", "Polka-dotted", "Chunky"]
+prep          = ["pan-fried", "sauteed", "steamed", "roasted", "baked", "boiled", "broiled", "fried",
+                "mashed", "breaded", "scorched", "barbecue", "slow-roasted", "braised", "chopped", "raw",
+                "sous vide", "poached"]
+rare_animals  = ["panda", "unicorn", "manatee", "wooly mammoth", "bald eagle", "gorilla", "emperor penguin",
+                "blue whale", "hammerhead shark", "toucan", "chimpanzee", "hummingbird", "sea turtle", "rhino",
+                "monarch butterfly", "sloth"]
 
 # Print menu items in an ordered list based on requested number of items by user
 count = 0
 while count < menu_items
-	count += 1
-	puts "#{count}. #{adj.sample} #{prep.sample} #{rare_animals.sample}"
+  count += 1
+  puts "#{count}. #{adj.sample} #{prep.sample} #{rare_animals.sample}"
 end
